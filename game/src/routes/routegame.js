afff7f2681c88b3679b1876933f60dfd65f99489
soufiane <soufianeajaite2@gmail.com>
2025-02-24 20:57:17 +0000

    -

--- game/src/routes/routegame.js
+++
@@ -1,8 +0,0 @@
-import {getAllGames, getGameById, createGame, cancelGame} from "../controllers/controller.game.js"
-
-export const gameRoutes = function(fastify, options){
-    fastify.get('/', getAllGames),
-    fastify.get('/:id', getGameById),
-    fastify.post('/', createGame),
-    fastify.delete('/:id', cancelGame)
-}
