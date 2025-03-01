479877f5552071861c7203cc368a6f4b36224fb3
soufiane <soufianeajaite2@gmail.com>
2025-03-01 21:54:36 +0000

    apis for game management good, still to add auth verification and validation of users and tournement

--- game/src/routes/routegame.js
+++ game/src/routes/routegame.js
@@ -1,8 +1,9 @@
-import {getAllGames, getGameById, createGame, cancelGame} from "../controllers/controller.game.js"
+import {getAllGames, getGameById, createGame, cancelGame, updateGame} from "../controllers/controller.game.js"
 
 export const gameRoutes = function(fastify, options){
     fastify.get('/', getAllGames),
     fastify.get('/:id', getGameById),
     fastify.post('/', createGame),
+    fastify.put('/:id', updateGame),
     fastify.delete('/:id', cancelGame)
 }
