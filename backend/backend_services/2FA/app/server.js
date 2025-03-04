27fb7c2d692768d7d3bd07835490de0a1e2e9126
7h3Y055 <youssefbouchmama18@outlook.com>
2025-03-04 03:58:05 +0000

    tmp

--- backend/backend_services/2FA/app/server.js
+++ backend/backend_services/2FA/app/server.js
@@ -28,7 +28,7 @@ fastify.register(multipart);
 fastify.register(routes, { prefix: '/api/2fa/' });
 
 // Run the server!
-fastify.listen({ port: 3002, host: '0.0.0.0' }, function (err, address) {
+fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
