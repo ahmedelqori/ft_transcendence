4ea6f025afe8a3e70f82c68c70c88e831718b828
ael-qori <ael-qori@c3r10p7.1337.ma>
2025-03-10 16:45:45 +0000

    set style attr events

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,18 @@
 import * as Uccello from "./uccello/Uccello.js";
-console.log(Uccello.createFragment(["Hello", Uccello.createElement("div", {}, ["Hello"])]));
+const ROOT = document.getElementById("root");
+const vdom = Uccello.createFragment([
+    "Hello",
+    Uccello.createElement("div", {
+        on: {
+            dblclick: () => console.log("1234"),
+        },
+    }, [
+        "Hello",
+        Uccello.createElement("div", {
+            on: {
+                click: () => console.log("hi"),
+            },
+        }, ["Hello"]),
+    ]),
+]);
+Uccello.mountDOM(vdom, ROOT);
--- srcs/index.ts
+++ srcs/index.ts
@@ -1,5 +1,29 @@
 import * as Uccello from "./uccello/Uccello.js";
 
-console.log(
-  Uccello.createFragment(["Hello", Uccello.createElement("div", {}, ["Hello"])])
-);
+const ROOT = document.getElementById("root") as HTMLElement;
+
+const vdom = Uccello.createFragment([
+  "Hello",
+  Uccello.createElement(
+    "div",
+    {
+      on: {
+        dblclick: () => console.log("1234"),
+      },
+    },
+    [
+      "Hello",
+      Uccello.createElement(
+        "div",
+        {
+          on: {
+            click: () => console.log("hi"),
+          },
+        },
+        ["Hello"]
+      ),
+    ]
+  ),
+]);
+
+Uccello.mountDOM(vdom, ROOT);
