c03f5cbd61965c512c7b76f7013857837c1c4559
ael-qori <ael-qori@e2r8p2.1337.ma>
2025-04-13 18:58:47 +0100

    add Scroll to Friends

--- srcs/components/FriendsInterface/FriendsInterface.ts
+++ srcs/components/FriendsInterface/FriendsInterface.ts
@@ -7,15 +7,15 @@ import FriendItem from "./FirendItem/FriendItem.js";
 
 interface FriendsInterfaceState {
   friends: (string | null)[];
+  currentPage: number;
+  totalPages: number;
 }
 
 const FriendsInterface = defineComponent<FriendsInterfaceState>({
   onMounted(this: IComponent<FriendsInterfaceState>) {
-    // setTimeout(() => {
-    //   this.updateState({
-    //     friends: [...this.state.friends, "baarif", "aes-arg", "mbentahi"],
-    //   });
-    // }, 3000);
+    this.updateState({
+      totalPages: Math.ceil(this.state.friends.length / 9),
+    });
   },
   state() {
     return {
@@ -26,14 +26,32 @@ const FriendsInterface = defineComponent<FriendsInterfaceState>({
         "ybouchma",
         "relhamma",
         "zibnoukh",
-
         "baarif",
         "aes-arg",
         "mbentahi",
+        "user10",
+        "user11",
+        "user12",
+        "user13",
+        "user14",
+        "user15",
       ],
+      currentPage: 0,
+      totalPages: 1,
     };
   },
-  render(this: IComponent<FriendsInterfaceState>) {
+
+  render(
+    this: IComponent<FriendsInterfaceState> & {
+      goToPage: (index: number) => void;
+      handleScroll: (event: Event) => void;
+    }
+  ) {
+    const pages = [];
+    for (let i = 0; i < this.state.friends.length; i += 9) {
+      pages.push(this.state.friends.slice(i, i + 9));
+    }
+
     return createElement(
       "section",
       {
@@ -51,26 +69,93 @@ const FriendsInterface = defineComponent<FriendsInterfaceState>({
           "div",
           {
             class: [
+              "friends-container",
               "w-full",
               "h-full",
               "max-h-[720px]",
-              "grid",
-              "grid-cols-3",
-              "grid-rows-3",
-              "w-full",
-              "h-screen",
-              "gap-4",
+              "overflow-x-auto",
+              "flex",
+              "snap-x",
+              "snap-mandatory",
+              "scrollbar-hide",
+              "overflow-scroll",
+              "overflow-x-hidden",
+              "[&::-webkit-scrollbar]:w-1",
+              "pr-2",
+              "[&::-webkit-scrollbar-track]:rounded-full",
+              "[&::-webkit-scrollbar-track]:bg-gray-100",
+              "[&::-webkit-scrollbar-thumb]:rounded-full",
+              "[&::-webkit-scrollbar-thumb]:bg-gray-300",
+              "dark:[&::-webkit-scrollbar-track]:bg-transparent",
+              "dark:[&::-webkit-scrollbar-thumb]:bg-[#ddf247]",
+              "dark:[&::-webkit-scrollbar-thumb]:bg-opacity-[70%]",
             ],
+            onScroll: this.handleScroll,
           },
-
-          [
-            ...this.state.friends.map((e) =>
-              createElement(FriendItem, { username: e })
-            ),
-          ]
+          pages.map((pageUsers) =>
+            createElement(
+              "div",
+              {
+                class: [
+                  "grid",
+                  "grid-cols-3",
+                  "grid-rows-3",
+                  "gap-4",
+                  "w-full",
+                  "h-full",
+                  "flex-shrink-0",
+                  "snap-center",
+                ],
+              },
+              pageUsers.map((username) =>
+                createElement(FriendItem, { username })
+              )
+            )
+          )
         ),
+        createElement("div", {
+          class: [
+            "absolute",
+            "bottom-4",
+            "left-1/2",
+            "transform",
+            "-translate-x-1/2",
+            "flex",
+            "space-x-2",
+            "z-10",
+          ],
+        }),
       ]
     );
+  },
+  goToPage(this: IComponent<FriendsInterfaceState>, pageIndex: number) {
+    if (pageIndex >= 0 && pageIndex < this.state.totalPages) {
+      const container = document.querySelector(".friends-container");
+      if (container) {
+        container.scrollTo({
+          left: pageIndex * container.clientWidth,
+          behavior: "smooth",
+        });
+
+        this.updateState({
+          currentPage: pageIndex,
+        });
+      }
+    }
+  },
+  handleScroll(this: IComponent<FriendsInterfaceState>, event: Event) {
+    const container = event.target as HTMLElement;
+    if (container) {
+      const pageWidth = container.clientWidth;
+      const scrollPosition = container.scrollLeft;
+      const newPage = Math.round(scrollPosition / pageWidth);
+
+      if (newPage !== this.state.currentPage) {
+        this.updateState({
+          currentPage: newPage,
+        });
+      }
+    }
   },
 });
 
