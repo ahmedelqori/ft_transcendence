1250238a3d7807e6d4c6720ee63d4fe099d0f58d
7h3Y055 <youssefbouchmama18@outlook.com>
2025-05-12 08:35:36 +0000

    implement websocket authentication middleware

--- infra/backend_services/notification/app/controllers/notification.js
+++ infra/backend_services/notification/app/controllers/notification.js
@@ -5,8 +5,8 @@ export default async function notification(connection, req) {
     console.log('New connection:');
     connections.set(req.user.id, connection);
 
-    connection.on('close', () => {
-        console.log('Connection closed');
+    connection.on('close', (event) => {
+        console.log('Connection closed', event);
         connections.delete(req.user.id);
     });
 
--- infra/backend_services/notification/app/routes.js
+++ infra/backend_services/notification/app/routes.js
@@ -3,16 +3,16 @@ import markAsRead from "./controllers/markAsRead.js";
 import get_notif from "./controllers/get_notif.js";
 import auth from "./middlewares/auth.js";
 import notification from "./controllers/notification.js";
+import wsAuth from "./middlewares/wsAuth.js";
 
 
 export default async function routes(fastify) {
-    fastify.addHook('preHandler', auth);
-
-    fastify.post('/', new_notif);
-    fastify.get('/', get_notif);
-    fastify.delete('/', markAsRead);    
-    fastify.delete('/:id', markAsRead);
-    fastify.get('/ws', { websocket: true}, notification);
+    // Register auth as a preHandler hook for all routes except /ws using route-level options
+    fastify.post('/', { preHandler: auth }, new_notif);
+    fastify.get('/', { preHandler: auth }, get_notif);
+    fastify.delete('/', { preHandler: auth }, markAsRead);    
+    fastify.delete('/:id', { preHandler: auth }, markAsRead);
+    fastify.get('/ws', { websocket: true, preHandler: wsAuth }, notification);
 }
 
 
