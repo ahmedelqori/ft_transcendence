5cb268ff21d52428a41d4496bc92c978016b4415
7h3Y055 <youssefbouchmama18@outlook.com>
2025-05-27 19:36:22 +0000

    add Sender Object to tournamentInvite notif

--- infra/backend_services/tournament/app/controllers/invite_player.js
+++ infra/backend_services/tournament/app/controllers/invite_player.js
@@ -23,7 +23,11 @@ export default async function invite_player(req, res) {
                 return;
             }
             const invite_link =  `${process.env.DOMAIN}/api/tournament/${tournament_id}/join?code=${settings.code}`;
-            await notif(req, player_id, "tournamentInvite", {invite_link, tournament_id});
+            await notif(req, player_id, "tournamentInvite", {
+                invite_link,
+                tournament_id,
+                senderId: req.user.id,
+            });
         }
         catch (error) {
             console.error(error);
